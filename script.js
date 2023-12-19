84c03d7500cdf531034654d82bb32d2ad2100c1f
Barış AYDOĞDU <barisaydogdu76@gmail.com>
2023-12-19 23:18:47 +0300

    JavaScript Giriş Yapıldı.

---
+++ script.js
@@ -0,0 +1,107 @@
+// let js = 'amazing';
+// console.log("outline script added!")
+// console.log("Jonas")
+// console.log(23)
+
+// let firstName = "Baris"
+// let first = 'jonas'
+
+// //veriable name conventions 
+// let jonasAndMatilda = 'JM'
+// let $new = 27;
+
+// let PI = 3.1415;
+
+// let myFirstJob = 'Programmer'
+// let myCurrentJob = 'Gamer'
+
+//let job1 = 'programmer'
+// let job2 = 'Gamer'
+
+// let $break = '-----------------------------------------------------------------------'
+
+// console.log(firstName + " is KING!");
+// console.log(first);
+// console.log(jonasAndMatilda);
+// console.log(PI)
+// console.log(myFirstJob)
+// console.log(myCurrentJob)
+// console.log($break)
+
+// --------------------------------------------------
+
+//let javaScriptIsFun = true;
+//console.log(javaScriptIsFun)
+
+// typeof 
+// console.log(typeof javaScriptIsFun + "is javaScriptIsFun's type")
+// console.log(typeof job1)
+
+// console.log('value changed!')
+
+// javaScriptIsFun = 'YES!'
+// console.log(typeof javaScriptIsFun + "is javaScriptIsFun's type")
+
+// let year;
+// console.log(year)
+// console.log(typeof year + "is year's type ")
+
+// year = 2021;
+// console.log(typeof year)
+
+// console.log(typeof null + " is null's type")
+
+
+// //const - var 
+
+// const is never changed!! 
+// var is like let
+
+// let age = 30;
+// age = 31;
+
+// const birthday = 2002;
+
+// const job = 'programmer'
+//console.log(job)
+
+// var job1 = 'Gamer'
+// job1 = 'engineer'
+// console.log(job1)
+// console.log(typeof job1)
+
+
+// Types of Operators
+// const now = 2024;
+// const ageBaris = now - 2002;
+// const ageSarah = now - 2003;
+// console.log(ageBaris, ageSarah);
+
+// console.log(ageBaris * 2, ageSarah / 10, 2 ** 3)
+// console.log(ageSarah * ageBaris)
+
+// const firstName = 'Baris';
+// const lastName = 'Aydogdu';
+// console.log(firstName + ' ' + lastName)
+
+// // Assigment Operators
+// let x = 10 + 5;
+// x += 10;
+// x *= 4;
+// x++;
+// x--;
+// console.log(x)
+
+// // Comperison Operators
+
+// console.log(ageBaris > ageSarah); // > , < , <= ,=>
+// console.log(ageBaris == 22)
+
+// const isFullAge = ageSarah >= 18;
+const now = 2024;
+const ageBaris = now - 2002;
+const ageSarah = now - 2003;
+
+console.log(now - 2003 > now - 2002)
+
+
